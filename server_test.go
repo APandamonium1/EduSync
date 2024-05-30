88e131b7c76a3d6a96b943f2cc08a78f1829daa9
PandaJW <5168pandayoyo@gmail.com>
2024-05-30 10:37:24 +0800

    Changed server_test.go

--- server_test.go
+++ server_test.go
@@ -16,13 +16,34 @@ func ahandler(w http.ResponseWriter, r *http.Request) {
 func TestRouter(t *testing.T) {
 
 	// req := httptest.NewRequest("GET", "http://192.168.1.129:8080/3", nil)
-	req := httptest.NewRequest("GET", "http://127.0.0.1:8080/1", nil)
-	w := httptest.NewRecorder()
+	// req := httptest.NewRequest("GET", "http://127.0.0.1:8080/1", nil)
+	// w := httptest.NewRecorder()
 
-	ahandler(w, req)
+	// ahandler(w, req)
 
-	resp := w.Result()
+	// resp := w.Result()
 
-	fmt.Println(resp.StatusCode)
+	// fmt.Println(resp.StatusCode)
+	req, err := http.NewRequest("GET", "/3", nil)
+	fmt.Println(req.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(serverhome)
+
+	handler.ServeHTTP(rr, req)
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	// Check the response body is what we expect.
+	// expected := "Hi"
+	// if rr.Body.String() != expected {
+	// 	t.Errorf("handler returned unexpected body: got %v want %v",
+	// 		rr.Body.String(), expected)
+	// }
 
 }
