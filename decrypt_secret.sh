7b455104deae767c4666caddcaade98dd2b99ae4
PandaJW <5168pandayoyo@gmail.com>
2024-06-18 22:55:06 +0800

    forgot abt this

--- decrypt_secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASE_ADMIN_PASSPHRASE" \
---output $HOME/secrets/edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json.gpg
