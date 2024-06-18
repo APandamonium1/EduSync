3fc0c653612cb3c06f0f72803b66a82e3c915dc5
PandaJW <5168pandayoyo@gmail.com>
2024-06-19 00:38:12 +0800

    lets try this

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+# mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASE_ADMIN_PASSPHRASE" \
-# --output $HOME/secrets/edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json.gpg
 --output edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json.gpg
+# --output $HOME/secrets/edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json.gpg
+
+# Optionally, you can output the decrypted file path for verification
+echo "Decrypted file: edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json"
