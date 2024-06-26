c78a5e959b3a1122e526fd4cf702073a10595208
Jeyvian <117579078+PeanutBrrutter@users.noreply.github.com>
2024-06-27 00:03:38 +0800

    Landing page (#27)
    * added index.html
    
    * added success.html
    
    * added assets
    
    * added home.html
    
    * with all the codes
    
    * added js, html is working
    
    * IT FINALLY WORKS
    
    * tidied things
    
    * added role routing and role htmls
    
    * removed database.go
    
    * added database.go
    
    * added some file

--- main.go
+++ main.go
@@ -4,43 +4,30 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/pat"
+	"github.com/gorilla/mux"
 )
 
 func init() {
-	// validateJSON("edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json")
-	// jsonFilePath := "$HOME/secrets/edusync-7bd5e-firebase-adminsdk-x49uh-af084a6314.json"
-
-	// err := validateJSON(jsonFilePath)
-	// if err != nil {
-	// 	log.Fatalf("JSON validation failed: %v", err)
-	// }
-
-	// log.Println("JSON is valid.")
 	database()
 }
 
 func main() {
-	// http.HandleFunc("/1", serverhome)
-	// http.HandleFunc("/2", setCookieHandler)
-	// http.ListenAndServe(":8080", handler())
-	// http.ListenAndServeTLS("192.168.1.129:8080", "server.crt", "server.key", handler())
-
-	// err := http.ListenAndServeTLS(":8080", "server.crt", "server.key", handler())
-
 	// Load configuration
 	config, err := LoadConfig("config.json")
 	if err != nil {
 		log.Fatalf("could not load config: %v", err)
 	}
 
-	// Create a new router
-	router := pat.New()
+	router := mux.NewRouter()
+
+	// Serving static files
+	fs := http.FileServer(http.Dir("assets"))
+	router.PathPrefix("/assets").Handler(http.StripPrefix("/assets", fs))
 
 	// Set up authentication routes
 	AuthHandler(router, config)
+	RoleHandler(router)
 
-	// Start the HTTPS server with the provided certificate and key files
 	log.Println("listening on localhost:8080")
 	err = http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", router)
 	if err != nil {
